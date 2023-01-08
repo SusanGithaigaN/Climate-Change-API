d4d6145c87e3fefa2f96d751fdd246414ea0efa9
SusanGithaigaN <susan.githaiga@student.moringaschool.com>
2023-01-08 09:52:01 +0300

    testing

--- index.js
+++ index.js
@@ -6,8 +6,59 @@ const express = require('express');
 const axios = require('axios');
 // initialize cheerio
 const cheerio = require('cheerio');
+const { response } = require('express');
 
 // call express and put the express app in a variable
 const app = express();
+
+// create an array of newspapers to be scraped
+const newspapers = [
+    {
+        name:"thetimes",
+        address : "https://www.thetimes.co.uk/environment/climate-change"
+    },
+    {
+        name:"guardian",
+        address : "https://www.theguardian.com/environment/climate-crisis",
+    },
+    {
+        name:"telegraph",
+        address : "https://www.telegraph.co.uk/climate-change",
+    }
+];
+
+// create an array where the Guardian climate data will be stored
+const articles = [];
+// loop through each item in the newspapers array
+newspapers.forEach(newspaper => {
+    axios.get(newspaper.address)
+    .then(response =>{
+        const html = response.data
+        const $ = cheerio.load(html)
+
+
+        $('a:contains("climate")', html).each(function(){
+           const title = $(this).text();
+            const url = $(this).attr('href');
+
+            articles.push({
+                title, 
+                url,
+                source:newspaper.name
+            })
+        })
+    })
+});
+
+// start scraping the webpage
+app.get('/', (reg,res) =>{
+    res.json('Welcome to my Climate Change News API');
+});
+
+// scrape the internet to get data
+app.get('/news', (req,res)=>{
+   res.json(articles);
+})
+
 // listen to our port
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
